ae5328efe75090c8cda514fda8f281707ed61eb8
Adelina <anghelescuadelina22@stud.ase.ro>
2026-01-03 15:22:41 +0200

    Connected controller methods to book routes with validations

--- API/routes/books.js
+++ API/routes/books.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const {getAllBooks, getBookById, createBook, updateBook, deleteBook} = require('../controllers/booksController');
+const {bookValidaton}   = require('../validators/booksValidator');
 
-// Rută de test pentru cărți
-router.get('/', (req, res) => {
-    res.json({ message: "Aici vor apărea toate cărțile din bibliotecă" });
-});
+router.get('/', getAllBooks);
+router.get('/:id', getBookById);
+
+router.post('/', bookValidaton, createBook);
+router.put('/:id', bookValidaton, updateBook);
+router.delete('/:id', deleteBook);
+
 
 module.exports = router;
