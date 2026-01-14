e0288b85db108cc3db0e0158bc87f3bbb2c73cf9
Adelina <anghelescuadelina22@stud.ase.ro>
2026-01-14 15:29:13 +0200

    Implemented the protection for sensitive routes in API/routes/books.js

--- API/routes/books.js
+++ API/routes/books.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const {getAllBooks, getBookById, createBook, updateBook, deleteBook} = require('../controllers/booksController');
 const {bookValidaton}   = require('../validators/booksValidator');
-
+const { validateToken } = require('../middleware/auth');
 //Public routes
 router.get('/', getAllBooks);
 router.get('/:id', getBookById);
 
 //Protected routes
-router.post('/', bookValidaton, createBook);
-router.put('/:id', bookValidaton, updateBook);
-router.delete('/:id', deleteBook);
+router.post('/', validateToken, bookValidaton, createBook);
+router.put('/:id', validateToken, bookValidaton, updateBook);
+router.delete('/:id', validateToken, deleteBook);
 
 
 module.exports = router;
