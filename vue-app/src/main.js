5a7e838f9ab69cfe2e66eca87ae726f3dc8f5f63
Adelina <anghelescuadelina22@stud.ase.ro>
2026-01-19 14:40:59 +0200

    Initialized the store in main.js

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -3,10 +3,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
 
+
 app.use(createPinia())
+
+const authStore = useAuthStore()
+authStore.checkAuth()
+
 app.use(router)
 
 app.mount('#app')
