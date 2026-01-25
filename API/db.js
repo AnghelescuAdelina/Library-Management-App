df76ac06f750f7fccf29b82e1df72ff08432d3c2
Adelina <anghelescuadelina22@stud.ase.ro>
2026-01-25 21:20:52 +0200

    Added the firebase account in .env

--- API/db.js
+++ API/db.js
@@ -1,8 +1,9 @@
 const admin = require('firebase-admin');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
  
 function initializeFirestore() {
     admin.initializeApp({
-        credential: admin.credential.cert(require('./serviceAccount.json'))
+        credential: admin.credential.cert(serviceAccount)
     });
 
     return admin.firestore();
